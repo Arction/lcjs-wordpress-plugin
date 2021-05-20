c953b41b1ae95f87a825623b32e3a897d03afcda
Chetana Kamble <chetana.kamble@lightningchart.com>
2021-05-20 12:02:00 +0530

    final-step

--- lcjs.php
+++ lcjs.php
@@ -7,7 +7,7 @@ and embed interactive charts into your WordPress posts and pages.
 Author: Lightningchart ,arction
 */
 require_once plugin_dir_path(__FILE__) . 'includes/mfp-functions.php';
-define( 'LC_JS_IIFE_URL', 'https://unpkg.com/@arction/lcjs@2.2.1/dist/lcjs.iife.js' );
+define( 'LC_JS_IIFE_URL', 'https://unpkg.com/@arction/lcjs@3.0.0/dist/lcjs.iife.js' );
 define( 'LC_JS_IIFE_URL', 'https://unpkg.com/@arction/xydata@1.4.0/dist/xydata.iife.js' );
 ?>
 <?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
